7ce64a0d4e70b59791e6a106f7df8c6c6e8d6825
Ferro Fang <fzinfz@gmail.com>
2016-11-09 12:34:29 +0000

    update

---
+++ install.sh
@@ -0,0 +1,1 @@
+curl -fsSL https://get.docker.com/ | sh
---
+++ mem.sh
@@ -0,0 +1,1 @@
+for line in `docker ps | awk '{print $1}' | grep -v CONTAINER`; do docker ps | grep $line | awk '{printf $NF" "}' && echo $(( `cat /sys/fs/cgroup/memory/docker/$line*/memory.usage_in_bytes` / 1024 / 1024 ))MB ; done
