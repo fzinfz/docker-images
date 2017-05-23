919c4ebe318c6401657a2abdff730394ae311e59
Ferro Fang <fzinfz@gmail.com>
2017-05-23 07:04:26 +0000

    update

--- build.sh
+++ build.sh
@@ -1,1 +1,15 @@
-docker build $2 -t fzinfz/$1:latest $1 
+if [ -z ${1+x} ]; then
+cat << EOF 
+Usage:
+	1. Create <folder_name>/Dockerfile[-<tag>] file
+	2. ./build.sh <folder_name> [tag]	
+Examples: 
+	./build.sh ubuntu
+	./build.sh jupyter jvm
+EOF
+
+elif [ -z ${2+x} ]; then
+	docker build -t fzinfz/$1:latest $1 
+else
+	docker build -f $1/Dockerfile-$2 -t fzinfz/$1:$2 $1
+fi
