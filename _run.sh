609d273a3edf3a724bf696a116738feab27ff60e
Ferro Fang <fzinfz@gmail.com>
2017-06-16 06:55:29 +0000

    update

--- _run.sh
+++ _run.sh
@@ -1,22 +1,32 @@
 #!/bin/bash
 
 n=$1
+shift
 docker stop $n 
 docker rm $n
 
-case $n in
-  rabbitmq | amqp ) i="relaxart/rabbitmq-server";;
-  ubuntu ) i="fzinfz/ubuntu";;
-esac
-shift
+mode_d='-d --restart unless-stopped'
+mode_i='-it --rm'
 
-mode='-d --restart unless-stopped'
-if [ $# -gt 0 ]; then
-	mode=$1
-	shift
-fi
+case $n in
+  rabbitmq | amqp ) 
+	i="relaxart/rabbitmq-server"
+	mode=$mode_d	
+	;;
+  ubuntu ) 
+	i="fzinfz/ubuntu"
+	mode=$mode_i
+	cmd=bash
+	;;
+  * )
+	if [ $# -gt 0 ]; then
+		mode=$1
+		shift
+		cmd="$*"
+	fi
+esac
 
 export IFS=' '
 docker run --name $n \
     --net host \
-    $mode $i sh -c "$*" 
+    $mode $i sh -c "$cmd" 
