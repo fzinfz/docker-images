7b9d533efb72c6480277688bef3c232b55345c2a
Ferro Fang <fzinfz@gmail.com>
2017-06-16 16:05:55 +0800

    update

--- _/stats.sh
+++ _/stats.sh
@@ -1,3 +1,8 @@
 # https://github.com/moby/moby/issues/20973
 
 docker stats $(docker ps --format={{.Names}}) --no-stream
+
+# copied from https://pastebin.com/xzu1VK6Y
+
+for line in `docker ps | awk '{print $1}' | grep -v CONTAINER`; do docker ps | grep $line | awk '{printf $NF" "}' && echo $(( `cat /sys/fs/cgroup/memory/docker/$line*/memory.usage_in_bytes` / 1024 / 1024 ))MB ; done
+
