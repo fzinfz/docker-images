69f9c8e62dbc60db76fecbc35d5c1bc994f735ad
Ferro Fang <fzinfz@gmail.com>
2017-06-16 19:58:13 +0800

    update

--- ELK_logstash.sh
+++ ELK_logstash.sh
@@ -7,9 +7,14 @@ docker rm $n
 docker run --name $n \
 	--net host \
 	-d --restart unless-stopped \
+	-e amqp_host="$amqp_host_ip" \
+	-e amqp_vhost="/" \
+	-e amqp_user=guest \
+	-e amqp_passwd=guest \
+	-e CONFIG_RELOAD_AUTOMATIC=true \
 	--add-host="elasticsearch:127.0.0.1" \
 	-v $(pwd)/${n}.conf.d/pipeline/:/usr/share/logstash/pipeline/ \
-       	docker.elastic.co/logstash/logstash:5.4.1
+       	docker.elastic.co/logstash/logstash:5.4.1 
 
 echo Usage: https://www.elastic.co/guide/en/logstash/current/_pulling_the_image.html
 echo Logging: http://localhost:9600/_node/?pretty
