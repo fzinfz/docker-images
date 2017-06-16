0f1b8f4ab15c4b0bddf600f0645f1985f9bd126a
Ferro Fang <fzinfz@gmail.com>
2017-06-16 20:38:31 +0800

    add nfprobe

--- _/stats.sh
+++ _/stats.sh
@@ -1,8 +1,7 @@
 # https://github.com/moby/moby/issues/20973
-
 docker stats $(docker ps --format={{.Names}}) --no-stream
 
+printf "\nMemory:\n"
 # copied from https://pastebin.com/xzu1VK6Y
-
 for line in `docker ps | awk '{print $1}' | grep -v CONTAINER`; do docker ps | grep $line | awk '{printf $NF" "}' && echo $(( `cat /sys/fs/cgroup/memory/docker/$line*/memory.usage_in_bytes` / 1024 / 1024 ))MB ; done
 
