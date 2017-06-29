f81efe5498a3db8287b6e8415b559fa15b199868
Ferro Fang <fzinfz@gmail.com>
2017-06-30 01:51:01 +0800

    update

--- _/mem.sh
+++
@@ -1,3 +0,0 @@
-# copied from https://pastebin.com/xzu1VK6Y
-
-for line in `docker ps | awk '{print $1}' | grep -v CONTAINER`; do docker ps | grep $line | awk '{printf $NF" "}' && echo $(( `cat /sys/fs/cgroup/memory/docker/$line*/memory.usage_in_bytes` / 1024 / 1024 ))MB ; done
