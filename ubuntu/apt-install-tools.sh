c7c5ecf8486e76acd52540f6342b4bb7ae732de6
fzinfz <fzinfz@gmail.com>
2017-07-03 12:47:40 +0800

    Update apt-install-tools.sh

--- ubuntu/apt-install-tools.sh
+++ ubuntu/apt-install-tools.sh
@@ -3,6 +3,5 @@ apt-get update && \
     apt-utils software-properties-common python-software-properties \
     net-tools bridge-utils dnsutils tcpdump  mtr nmap nethogs traceroute bmon iputils-ping nload iftop \
     locate ncdu vim aria2 curl wget htop git pciutils unzip \
-    iperf3 sysbench \
-    crunch hydra ncrack
+    iperf3 sysbench
 
