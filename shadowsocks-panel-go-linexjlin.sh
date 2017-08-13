91e34dd9f3c1f6bb066e4bbbe7dae63ba955b725
Ferro Fang <fzinfz@gmail.com>
2017-08-14 00:59:43 +0800

    re-arrange ss projects

--- shadowsocks-panel-go-linexjlin.sh
+++
@@ -1,6 +0,0 @@
-docker run \
-	--net host -it \
-	-v $(pwd)/shadowsocks.conf.d/panel-go-linexjlin.config.json:/go/src/github.com/linexjlin/ss-web-manager/config.json \
-	-v $(pwd)/../docker-data/redis/redis.sock:/go/src/github.com/linexjlin/ss-web-manager/redisDB/redis.sock \
-	fzinfz/shadowsocks:panel-go-linexjlin
-
