20b54e2bc1449541c0e7ad9e0ec6463b0afa088a
Ferro Fang <fzinfz@gmail.com>
2017-11-24 07:57:24 -0500

    update

--- init.sh
+++ init.sh
@@ -1,57 +1,31 @@
-export docker_run_d='-d --restart unless-stopped'
-export mode_d=$docker_run_d
-
-export mode_i='--rm -it'
-
-export docker_run_host='--privileged --cap-add=ALL -it -v /dev:/dev -v /lib/modules:/lib/modules --pid=host --ipc=host --net host -v /:/host'
-export mode_host=$docker_run_host
-
-cat << EOL
-dockerfile code snippets:
-
-RUN apt update && apt install -y 
-    --no-install-recommends && rm -r /var/lib/apt/lists/*
-
-RUN apk add --no-cache --virtual .build-deps'  
-    && apk del .build-deps
-EOL
-
-docker_vim_py() {
-    docker run -it --rm -v $(pwd):/src fzinfz/tools:vim-py
-}
-
-docker_bash() {
-    docker run -it --rm -v $(pwd):/data $mode_host fzinfz/tools
-}
-
 docker_rm_all_stopped() {
-# docker ps -a | egrep 'Exited|Created' | awk '{print $1}' | xargs --no-run-if-empty docker rm
+    # docker ps -a | egrep 'Exited|Created' | awk '{print $1}' | xargs --no-run-if-empty docker rm
     docker container prune
 }
 
 docker_rmi_all() {
-     docker rmi $(docker images -q)
+    docker rmi $(docker images -q)
 }
 
 docker_rmi_all_unsed() {
-     docker image prune
+    docker image prune
 }
 
 docker_stop_all() {
-     docker kill $(docker ps -q)
+    docker kill $(docker ps -q)
 }
 
 docker_logs--container--regex() {
-     docker logs $1  2>&1 | grep -P $2
+    docker logs $1  2>&1 | grep -P $2
 }
 
 docker_rm_all() {
-     sh stop_all.sh
+    sh stop_all.sh
     docker rm $(docker ps -a -q)
 }
 
 docker_clean_all() {
-     docker system prune
+    docker system prune
 }
 
 docker_stop_N_rm--egrep() {
@@ -87,3 +61,30 @@ docker_stats() {
         echo $(( `cat /sys/fs/cgroup/memory/docker/$line*/memory.usage_in_bytes` / 1024 / 1024 ))MB ; \
     done
 }
+
+
+mode_d='-d --restart unless-stopped'
+mode_host='--privileged --cap-add=ALL -it -v /dev:/dev -v /lib/modules:/lib/modules --pid=host --ipc=host --net host -v /:/host'
+
+docker_run_vim_py() {
+    docker run -it --rm -v $(pwd):/src fzinfz/tools:vim-py $1
+}
+
+docker_run_bash() {
+    docker run -it --rm -v $(pwd):/data $mode_host fzinfz/tools
+}
+
+docker_run_golang_rmit(){
+    docker run --rm -it -v $PWD:/data  golang /bin/bash
+}
+
+docker_run_unifi_d() {
+    docker run -d --init --restart unless-stopped \
+	-p 8080:8080 -p 8443:8443 -p 3478:3478/udp -p 10001:10001/udp \
+	-e TZ='Asia/Shanghai' \
+	-e RUNAS_UID0='true' \
+	-v $PWD/../docker-data/unifi:/unifi \
+	--name unifi jacobalberty/unifi
+}
+
+
