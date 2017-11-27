e74a05e05d1215ebe68dc75ff12b2e4398515ceb
Ferro Fang <fzinfz@gmail.com>
2017-11-27 16:58:39 +0800

    update

--- ELK_elasticsearch.sh
+++ ELK_elasticsearch.sh
@@ -4,16 +4,18 @@ n=$(basename $0 .sh)
 docker stop $n 
 docker rm $n
 
+data_path=$(pwd)/../docker-data/$n
+mkdir -p $data_path
+chmod 777 $data_path
+
 docker run --name $n \
 	--net host \
 	-d --restart unless-stopped \
 	-e "discovery.type=single-node" \
 	-e ES_JAVA_OPTS="-Xms3g -Xmx3g" \
+	-v $data_path:/usr/share/elasticsearch/data \
 	docker.elastic.co/elasticsearch/elasticsearch:$ELK_version
 
-#	-v $(pwd)/../docker-data/$n:/usr/share/elasticsearch/data \
-	#-e http.host=0.0.0.0 -e transport.host=127.0.0.1 \
-	#-e path.repo=/usr/share/elasticsearch/data \
 cat << EOL 
 
 Doc: 			https://www.elastic.co/guide/en/elasticsearch/reference/current/docker.html
